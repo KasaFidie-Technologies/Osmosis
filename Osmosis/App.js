f98b5dc9a9e5a4aca56726ac4ab7981e107aa900
Aliu Tijani <aliutijani21@gmail.com>
2024-03-28 17:11:50 +0000

    updated components to App.js

--- Osmosis/App.js
+++ Osmosis/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//import Login from "./screens/login";
+import Signup from "./screens/Signup";
+import Signin from './screens/Signin';
+import VerificationScreen from './screens/authentication';
+import loading from "./screens/Welcome";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <loading/>
+     
     </View>
   );
 }
@@ -13,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 15,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
